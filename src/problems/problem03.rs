1e0e4e4ce310479299ea6d1e98d9adfa58dab24b
Simon Duchastel <simon@duchastel.com>
2024-01-14 07:17:10 -0800

    problem 3 part 2

--- src/problems/problem03.rs
+++ src/problems/problem03.rs
@@ -3,6 +3,8 @@ use std::convert::TryFrom;
 
 use crate::file::{read_file};
 
+const NUM_ADJACENT_PARTS_FOR_GEAR_RATIO: u32 = 2;
+
 pub fn problem03_part_1(input_file: &str) -> Result<(), Box<dyn Error>> {
     let engine_schematic = match parse_engine_schematic(input_file) {
         Ok(schematic) => schematic,
@@ -20,6 +22,9 @@ pub fn problem03_part_2(input_file: &str) -> Result<(), Box<dyn Error>> {
         Ok(schematic) => schematic,
         Err(err) => return Err(err),
     };
+
+    let sum_of_gear_ratios: u32 = find_gear_ratios(&engine_schematic, NUM_ADJACENT_PARTS_FOR_GEAR_RATIO).iter().sum();
+    println!("Sum of gear ratios: {}", sum_of_gear_ratios);
 
     return Ok(());
 }
@@ -112,6 +117,48 @@ fn find_adjacent_part_numbers(engine_schematic: &EngineSchematic) -> Vec<u32> {
     }
 
     return adjacent_part_numbers;
+}
+
+
+
+fn find_gear_ratios(engine_schematic: &EngineSchematic, num_adjacent_parts: u32) -> Vec<u32> {
+    let mut gear_ratios = Vec::new();
+
+    for symbol in engine_schematic.symbols.iter() {
+        if symbol.symbol == '*' {
+            // we multiply the gear ratio by each adjacent part.
+            // For the first part, 1 * part = part. For each subsequent
+            // part, the gear ratio increases multiplicatively as expected.
+            let mut gear_ratio = 1;
+            let mut current_adjacent_parts = 0;
+            
+            for part in engine_schematic.part_numbers.iter() {
+                let left_boundary = if part.start_column == 0 { 0 } else { part.start_column - 1 };
+                let right_boundary = part.end_column + 1;
+                let top_boundary = if part.row == 0 { 0 } else { part.row - 1 };
+                let bottom_boundary = part.row + 1;
+                if symbol.column >= left_boundary && symbol.column <= right_boundary
+                    && symbol.row >= top_boundary && symbol.row <= bottom_boundary 
+                {
+                    gear_ratio *= part.number;
+                    current_adjacent_parts += 1;
+                    
+                    if current_adjacent_parts > num_adjacent_parts {
+                        // short circuit if we've exceeded the # of adjacent parts we're looking for
+                        break;
+                    }
+                }
+            }
+
+            if current_adjacent_parts == num_adjacent_parts {
+                // if the number of adjacent parts found is equal to the number we're
+                // looking for, add the gear ratio to the list
+                gear_ratios.push(gear_ratio);
+            }
+        }
+    }
+
+    return gear_ratios;
 }
 
 #[derive(Debug)]

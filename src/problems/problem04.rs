8513524de467b45ac27746a87d4816137d409a23
Simon Duchastel <simon@duchastel.com>
2024-01-14 08:57:40 -0800

    Problem 4 part b
    (This is an inefficient implementation, and runs slowly)

--- src/problems/problem04.rs
+++ src/problems/problem04.rs
@@ -1,6 +1,9 @@
+use std::collections::HashMap;
 use std::error::Error;
 
 use crate::file::{read_file};
+
+const VALUE_POWER: u32 = 2;
 
 pub fn problem04_part_1(input_file: &str) -> Result<(), Box<dyn Error>> {
     let input = read_file(input_file)?;
@@ -22,9 +25,25 @@ pub fn problem04_part_2(input_file: &str) -> Result<(), Box<dyn Error>> {
     let input = read_file(input_file)?;
     let lines: Vec<&str> = input.lines().collect();
 
+    let mut num_cards: u32 = 0;
+    let mut bonus_cards = HashMap::new();
     for line in lines {
         let card = parse_card(&line)?;
+        let times_to_process = 1 + *bonus_cards.get(&card.id).get_or_insert(&0); // 1 for the original card + bonus cards
+
+        for _ in 0..times_to_process {
+            num_cards += 1;
+
+            // for each nth win of this card, add a bonus card n further in the stack
+            for bonus in 0..card.matching_numbers() {
+                let bonus_id = card.id + bonus + 1;
+                let old_value = *bonus_cards.get(&bonus_id).get_or_insert(&0); // offset by 1 to account for 0-index
+                bonus_cards.insert(bonus_id, old_value + 1);
+            }
+        }
     }
+
+    println!("Number of total cards: {}", num_cards);
 
     Ok(())
 }
@@ -78,17 +97,22 @@ struct Card {
 
 impl Card {
     fn value(&self) -> u32 {
-        let mut value = 0;
+        let num = self.matching_numbers();
+        if num > 0 {
+            VALUE_POWER.pow(num - 1)
+        } else {
+            0
+        }
+    }
+
+    fn matching_numbers(&self) -> u32 {
+        let mut matching_numbers = 0;
         for number in self.present_numbers.iter() {
             if self.winning_numbers.contains(&number) {
-                if value == 0 {
-                    value = 1;
-                } else {
-                    value *= 2;
-                }
+                matching_numbers += 1;
             }
         }
         
-        value
+        matching_numbers
     }
 }
